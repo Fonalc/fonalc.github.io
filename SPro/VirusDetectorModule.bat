8912dba724baab5e39c87b42468b9e201d8ab66e
Fonalc <124433429+Fonalc@users.noreply.github.com>
2023-09-09 23:46:24 +0100

    uh9we

--- SPro/VirusDetectorModule.bat
+++ SPro/VirusDetectorModule.bat
@@ -1,14 +1,8 @@
-SET /p yn=Would you like to scan? (Y/N): 
-if %yn%==Y GOTO Scan
-EXIT
-if %yn%==N EXIT
-
-:Scan
-    ECHO  ______________________________________________________________
-    ECHO |                                                              |
-    ECHO |                           SProt                              |
-    ECHO |                                                              |
-    ECHO |     Scanning current directory, this may take a while...     |
-    ECHO |                                                              |
-    for /r %%i in (*) do ECHO Scanned "%%i"
-EXIT /B 0
+ECHO  ______________________________________________________________
+ECHO |                                                              |
+ECHO |                           SProt                              |
+ECHO |                                                              |
+ECHO |     Scanning current directory, this may take a while...     |
+ECHO |                                                              |
+ECHO |    LOGS:                                                     |
+for /r %%i in (*) do ECHO | Scanned "%%i"
